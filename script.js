b43320935370d072d36b2355281d10bee128dfe8
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-03-03 10:59:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function isSameType(value1, value2) {
+    if (Number.isNaN(value1) || Number.isNaN(value2)) {
+        return Number.isNaN(value1) && Number.isNaN(value2);
+    }
+    return typeof value1 === typeof value2;
+}
+
+function daysOfAYear(year) {
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        return 366;
+    }
+    return 365;
+}
